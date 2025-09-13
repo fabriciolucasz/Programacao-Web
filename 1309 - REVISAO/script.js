33368e7e3677c4824251a5d1808f70907ca0eb18
Fabricio Lucas <107938164+fabriciolucasz@users.noreply.github.com>
2025-09-13 09:59:20 -0300

    Organização do conteúdo e revisão

--- 1309 - REVISAO/script.js
+++ 1309 - REVISAO/script.js
@@ -1,1 +1,11 @@
+// Function
+function verificarIdade(idade) {
+  if (idade >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Arrow Function
 const verificarIdade = idade => idade >= 18;
